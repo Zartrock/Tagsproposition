27a5b8514e85243a4b54cf6dfef8bf683f4ab7b5
Jonas Zeff <69645784+Zartrock@users.noreply.github.com>
2022-04-25 10:25:10 +0200

    Update code.py

--- code.py
+++ code.py
@@ -1,17 +1,7 @@
 
 import pickle
-f = open("test.txt", "rb")
-print(f.read())
 
-f2 = open("lda_model.pkl", "rb")
-lda_model = pickle.load(f2)
-print('good')
 
-f1 = open("dictionary.pkl", "rb")
-id2word = pickle.load(f1)
-
-with open("dictionary.pkl", "rb") as f1:
-    id2word = pickle.load(f1)
 
 from sklearn.model_selection import train_test_split
 import pandas as pd
@@ -42,6 +32,13 @@ from gensim.models import CoherenceModel
 
 nltk.download('wordnet', r'C:\Users\jonas\PycharmProjects\pythonProject1\code\nltk_data')
 nltk.data.path.append(r'C:\Users\jonas\PycharmProjects\pythonProject1\code\nltk_data')
+
+f2 = open("lda_model.pkl", "rb")
+lda_model = pickle.load(f2)
+print('good')
+
+f1 = open("dictionary.pkl", "rb")
+id2word = pickle.load(f1)
 
 ##'''with open(r'C:/Users/jonas/Desktop/OC/P5/code/lda_model.pkl', 'rb') as file:
 ##   lda_model = pickle.load(file)
